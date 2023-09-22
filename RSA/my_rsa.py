1cf617ce477dc0acd5073f7c4db7ccf287466f26
José Alexandre Caldeira <jalexandre.caldeiraa@gmail.com>
2023-09-22 14:32:09 -0300

    adding the verify_decrypted_message function and changing input message in encrypt function

--- RSA/my_rsa.py
+++ RSA/my_rsa.py
@@ -5,7 +5,7 @@ from base64 import b64encode, b64decode
 from os import system
 
 def encrypt(public_key: int, product_of_prime_numbers: int) -> str:
-    message = input('Input your decrypted message: ')
+    message = verify_decrypted_message()
     ascii_message = [ord(char) for char in message]
     cipher_text_bytes = b''.join([pow(ascii_number, public_key, product_of_prime_numbers).to_bytes(64, byteorder='big') for ascii_number in ascii_message])
     compressed_cipher_text = compress(cipher_text_bytes)
@@ -20,3 +20,15 @@ def decrypt(private_key: int, product_of_prime_numbers: int) -> str:
     ascii_message_list = [int.from_bytes(block, byteorder='big') for block in blocks_list]
     decrypted_message = ''.join(chr(pow(ascii_number, private_key, product_of_prime_numbers)) for ascii_number in ascii_message_list)
     return decrypted_message
+
+def verify_decrypted_message() -> str:
+    while True:
+        message = input('\nInput your decrypted message: ')
+        if len(message) <= 128:
+            return message
+        else:
+            system('cls')
+            print('The decrypted message must have a maximum of 128 characters!')
+            input('\nPress the Enter key to insert a new message. . .')
+            system('cls')
+            print('ENCRYPTING A MESSAGE')
